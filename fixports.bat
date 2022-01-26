11a3da70648b1e3b0fc498fbde271ea6ca873994
Roy Underwood <roy@royunderwood.com>
2022-01-26 10:23:04 -0500

    fixports.bat

--- fixports.bat
+++ fixports.bat
@@ -1,2 +1,4 @@
+REM This is to reserve Quickbooks Ports from DNS
+
 int ipv4 add excludedportrange protocol=udp 55368 numberofports=25  
 
--- fixports2.bat
+++ fixports2.bat
@@ -1,1 +1,2 @@
+REM This is to reserve Quickbooks Ports from DNS
 int ipv4 add excludedportrange protocol=udp 60511 numberofports=25  
